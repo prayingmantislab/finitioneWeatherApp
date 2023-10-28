eee73f9f061b7aa552f98a5a20e3084ea31bbe7f
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-10-28 20:10:09 +0300

    added search icon

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -74,6 +74,10 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      shapes: {
+        'trapezoid': 'polygon(0 0, 100% 0, 75% 100%, 0% 100%)',
+      },
+      
     },
   },
   plugins: [require("tailwindcss-animate")],
