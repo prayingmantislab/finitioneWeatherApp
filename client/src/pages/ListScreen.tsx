f6f210924b2299d9f172b18fb67af4ee7972b88f
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-10-28 17:22:41 +0300

    improved error message

--- client/src/pages/ListScreen.tsx
+++ client/src/pages/ListScreen.tsx
@@ -51,24 +51,19 @@ function ListScreen({
         {error && <p>{error}</p>}
 
         <div className='mt-4'>
-          {
-            //if weather is empty return null else map over the weather array
-            //clear the input field after the search and the try again message after the search
-            weather.length === 1 && weather[0].cod === '404' ? (
-              // <p className='text-lg text-white mb-2'>
-              //   City not found, check your typing and try again.
-              // </p>
-              <p className='text-lg text-white mb-2'>{weather[0].message}</p>
-            ) : (
-              weather.map((data) => (
-                <WeatherCard
-                  onCardClick={() => onSelectedCityId(data.id)}
-                  key={data.id}
-                  weatherData={data}
-                />
-              ))
-            )
-          }
+          {weather.length === 1 && weather[0].cod === '404' ? (
+            <p className='text-lg text-white mb-2'>
+              {weather[0].message} Try again
+            </p>
+          ) : (
+            weather.map((data) => (
+              <WeatherCard
+                onCardClick={() => onSelectedCityId(data.id)}
+                key={data.id}
+                weatherData={data}
+              />
+            ))
+          )}
         </div>
       </div>
     </div>
