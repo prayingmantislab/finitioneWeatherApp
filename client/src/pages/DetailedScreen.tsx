42ef90105663dc1a02d2db612aa105d1e21fb208
Asi Meir <45923568+prayingmantislab@users.noreply.github.com>
2023-10-29 10:34:00 +0200

    added back button

--- client/src/pages/DetailedScreen.tsx
+++ client/src/pages/DetailedScreen.tsx
@@ -1,6 +1,7 @@
 import bgNightSky from '../assets/bg-night-sky.png';
 import house from '../assets/house.png';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 interface DetailedScreenProps {
   cityWeatherData: {
     id: number;
@@ -23,6 +24,9 @@ function DetailedScreen(props: DetailedScreenProps) {
   const { cityWeatherData } = props;
   const { weather, main, name } = cityWeatherData;
   const firstDescription = weather[0]?.description || '';
+  //add a back  button that renders the listScreen
+  //add a back button icon using fontawesome
+  //locate the icon in the top left corner
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
       <div
@@ -31,7 +35,14 @@ function DetailedScreen(props: DetailedScreenProps) {
           backgroundImage: `url(${bgNightSky})`,
         }}
       ></div>
-      <div className='bg-opacity-75 p-8 z-10 text-center'>
+      <div className='bg-opacity-75 z-10 justified-start'>
+        <button
+          onClick={() => window.location.reload()}
+          className='absolute top-0 left-0 text-white text-2xl m-4'
+        >
+          <FontAwesomeIcon icon={faArrowLeft} />{' '}
+        </button>
+
         <h2 className='text-3xl text-white mb-4'>{name}</h2>
         <p className='text-6xl text-white mb-4'>{Math.round(main.temp)}°</p>
         <p className='text-lg  text-white mb-4'>{firstDescription}</p>
